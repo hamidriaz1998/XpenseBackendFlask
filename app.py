33df71730a72a10b90ff284d33d73935437a0667
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-05-29 10:29:52 +0500

    chore: Register blueprints for API controllers in app.py

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask
 from extensions import db, migrate
 from config import Config
+import importlib
+import Controllers
+
+def register_blueprints(app):
+    for module_name in Controllers.__all__:
+        module = importlib.import_module(f'Controllers.{module_name}')
+        if hasattr(module, 'bp'):
+            app.register_blueprint(module.bp, url_prefix='/api')
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
     migrate.init_app(app, db)
+
+    register_blueprints(app)
+
     return app
 
 
