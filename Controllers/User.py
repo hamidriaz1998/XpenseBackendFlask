84cc9996430639a778dff9d7819d2d1c104c1a19
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-05-29 09:03:53 +0500

    add a simple user registration endpoint for testing

---
+++ Controllers/User.py
@@ -0,0 +1,17 @@
+from app import app, db
+from Models.User import UserModel
+from flask import request, jsonify
+import bcrypt
+
+@app.route('/user', methods=['POST', 'GET'])
+def user():
+    if request.method == 'POST':
+        data = request.get_json()
+        hash = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+        user = UserModel(name=data['name'], email=data['email'], password_hash=hash)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({'message': 'User created!'}), 201
+    elif request.method == 'GET':
+        users = UserModel.query.all()
+        return jsonify([user.__dict__ for user in users])
